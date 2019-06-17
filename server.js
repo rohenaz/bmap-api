c9b42e51e53026e4dbe123e75138d18ab19d1781
Satchmo <rohenaz@gmail.com>
2019-06-17 14:50:43 -0400

    use custom for cors, update neonplanaria, remove reference to fork

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 // Planarium
 const { planarium } = require('neonplanaria')
 const bitquery = require('bitquery')
+const cors = require('cors')
+
 planarium.start({
   name: 'BMAP',
   port: 80,
   onstart: async function() {
     let db = await bitquery.init({ url: 'mongodb://localhost:27017', address: 'planaria' })
     return { db: db }
+  },
+  custom: function(e) {
+    e.app.use(cors())
   },
   onquery: function(e) {
     let code = Buffer.from(e.query, 'base64').toString()
