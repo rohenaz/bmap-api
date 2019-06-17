641dd742fe48978672ddb2ae13b0e830abd7a966
Satchmo <rohenaz@gmail.com>
2019-06-17 16:37:11 -0400

    update neonplanaria for custom cors

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 // Planarium
 const { planarium } = require('neonplanaria')
 const bitquery = require('bitquery')
+const cors = require('cors')
 
 planarium.start({
   name: 'BMAP',
   port: 80,
   custom: function(e) {
-    e.app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*")
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Key")
-      next()
-    })
+    e.app.use(cors())
   },
   onstart: async function() {
     let db = await bitquery.init({ url: 'mongodb://localhost:27017', address: 'planaria' })
