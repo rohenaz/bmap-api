4f42bc59370ac25a3319cf4af97acab0cc2af01c
Satchmo <rohenaz@gmail.com>
2019-12-12 16:34:49 -0500

    wip

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # bmap-planaria
 BMAP BitDB for building B + MAP based Bitcoin apps.
 
+In a nutshell, BMAP planaria takes BOB transactions as an input, and filters for only transactions containing MAP attribute data. It then provides support for a number of known OP_RETURN protocols making queries very simple and easy to read.
+
+```
+{
+  find: {
+    "MAP.app": "tonicpow"
+  }
+}
+
+```
+TODO
+<a href="https://b.map.sv/q/">Example</a>
+
 # Warning
 *This planaria is a work in progress. It is not ready for production use.*
 It requires a fundemental solution to [this problem](https://github.com/interplanaria/planaria/issues/12) in order to be fully realized.
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const winston = require('winston')
 let db
 
 const logger = winston.createLogger({
-  level: 'error',
+  level: 'info',
   format: winston.format.json(),
   defaultMeta: { service: 'planaria' },
   transports: [
@@ -15,26 +15,28 @@ const logger = winston.createLogger({
     // - Write all logs error (and below) to `error.log`.
     //
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
+    new winston.transports.File({ filename: 'combined.log', options: { flags: 'w' } })
   ]
 })
 
 // Filter non BMAP txs. 
 // Returns bmap versions of each tx
-const bmapTransform = function (items) {
+const bmapTransform = async function (items) {
   let newItems = []
 
-  items.forEach(async (item) => {
+  await items.forEach(async (item) => {
     try {
       let bmapItem = await bmap.TransformTx(item)
       if (!bmapItem) {
         logger.log({level: 'error', message: 'failed to transform this ' + bmapItem })
         return []
       }
-      let hasMap = bmapItem.hasOwnProperty('MAP')
-      // let hasB = bmapItem.hasOwnProperty('B')
-      // let hasMeta = bmapItem.hasOwnProperty('METANET')
-      if (bmapItem && hasMap) {
+
+      // protocol whitelist
+      let list = ['AIP', 'B', 'BITCOM', 'BITPIC', 'BITKEY', 'HAIP', 'MAP', 'METANET', 'RON', 'SYMRE', '$']
+
+      let hasSupportedProtocol = Object.keys(bmapItem).some((k) => { return list.indexOf(k) !== -1 })
+      if (bmapItem && hasSupportedProtocol) {
         logger.log({ level: 'info', message: 'Storing BMAP ' + bmapItem.tx.h })
         newItems.push(bmapItem)
       }
@@ -51,12 +53,12 @@ const bmapTransform = function (items) {
 const connect = function(cb) {
   MongoClient.connect('mongodb://localhost:27017', {useNewUrlParser: true}, function(err, client) {
     if (err) {
-      logger.log({ level: 'info', message: 'DB Error. retrying...' })
+      logger.log({ level: 'info', message: 'db retrying...' })
       setTimeout(function() {
         connect(cb)
       }, 1000)
     } else {
-      db = client.db("planaria")
+      db = client.db('bmap')
       cb()
     }
   })
@@ -70,29 +72,66 @@ planaria.start({
     "q": {
       "find": { 
         "out.tape.cell.s": { 
-          "$in": ["19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut", "1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5"] 
+          "$in": ["19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut", "1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5", "13SrNDkVzY5bHBRKNu5iXTQ7K7VqTh5tJC", "18pAqbYqhzErT6Zk3a5dwxHtB9icv8jH2p", "1GvFYzwtFix3qSAZhESQVTz9DeudHZNoh1", "$"] 
         }
-      }
+      },
+      "project": { "out": 1, "tx": 1, "blk": 1, "in": 1}
     }
   },
   onmempool: async function(e) {
-    let bmaps = bmapTransform([e.tx])
-    await db.collection("u").insertMany(bmaps)
+    try {
+      let bmaps = await bmapTransform([e.tx])
+      if (bmaps.length > 0) {
+        await db.collection("u").insertMany(bmaps)
+      } else {
+        logger.log({ level: 'info', message: 'no bmaps ####### ' + bmaps })
+      }
+    } catch (e) {
+      logger.log({ level: 'error', message: 'onmempool: error transforming ' + e })
+    }
   },
   onblock: async function(e) {
-    await db.collection("u").deleteMany({ })
+    return new Promise(async (resolve, reject) => {
 
-    let bmaps = bmapTransform(e.tx)
-    try {
-      await db.collection("c").insertMany(bmaps)
-    } catch (err) {
-      logger.log({ level: 'error', message: 'error saving on block: ' + err + 'items: ' + bmaps.length })      
-    }
+      let bmaps
+      try {
+        bmaps = await bmapTransform(e.tx)
+      } catch (e) {
+        logger.log({ level: 'error', message: 'onblock: error transforming ' + e })
+      }
+      if (bmaps.length > 0) {
+        let ids = bmaps.map((t) => {
+          return t.tx.h
+        })
+        console.log('delete c')
+        await db.collection("c").deleteMany({
+          "tx.h": {
+            "$in": ids
+          }
+        })
 
-    if (e.mem.length) {
-      let bmapsMem = bmapTransform(e.mem)
-      await db.collection("u").insertMany(bmapsMem)
-    }
+        console.log("inserting", bmaps.length)
+        try {
+          await db.collection("c").insertMany(bmaps)
+        } catch (err) {
+          logger.log({ level: 'info', message: 'error saving on block: ' + err + ' items: ' + bmaps.length })      
+        }
+    
+        // if (e.mem.length) {
+        //   let bmapsMem = bmapTransform(e.mem)
+        //   await db.collection("u").insertMany(bmapsMem)
+        // }
+        // console.log("inserted u", bmapsMem)
+
+      } else {
+        logger.log({ level: 'info', message: 'no bmaps ####### ' + bmaps })
+      }
+
+      console.log("block End", e.height)
+      db.collection("u").deleteMany({}).then(() => {
+        resolve()
+      })
+    })
   },
   onstart: function(e) {
     if (process.env.NODE_ENV !== 'production') {
@@ -102,15 +141,30 @@ planaria.start({
     }
     return new Promise(async function(resolve, reject) {
       if (!e.tape.self.start) {
-        await planaria.exec("docker", ["pull", "mongo:latest"])
-        await planaria.exec("docker", ["run", "-d", "-p", "27017-27019:27017-27019", "-v", process.cwd() + (process.platform === 'win32' ? "/db" : "/db:/data/db"), "mongo:latest"])
+        logger.log({ level: 'info', message: 'Starting mongodb via docker. Platform ' + process.platform})
+        await planaria.exec("docker", ["pull", "mongo:4.0.4"])
+
+        try {
+          await planaria.exec("docker", ["run", "-d", "-p", "27017-27019:27017-27019", "-v", process.cwd() + "/db:/data/db", "mongo:4.0.4"])
+        } catch (e) {
+          logger.log({ level: 'error', message: 'Failed to start docker container for mongodb: ' + e})
+        }        
       }
-      connect(function() {
+      connect(async () => {
+        console.log("creating index")
+        await db.collection("c").createIndex({"tx.h": 1}, { unique: true})
+        await db.collection("c").createIndex({"blk.i": 1})
+        await db.collection("c").createIndex({"MAP.app": 1})
+        await db.collection("c").createIndex({"BITPIC": 1})
+        await db.collection("u").createIndex({"tx.h": 1}, { unique: true})
+        await db.collection("u").createIndex({"MAP.app": 1})
+        await db.collection("u").createIndex({"BITPIC": 1})
         logger.log({ level: 'info', message: 'Connected' })
         if (e.tape.self.start) {
-          db.collection("c").deleteMany({
+          await db.collection("c").deleteMany({
             "blk.i": { "$gt": e.tape.self.end }
-          }).then(resolve)
+          })
+          resolve()
         } else {
           resolve()
         }
