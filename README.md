fd3a515ee1f2eecb72c5cb0baee1dc486a8f5833
Luke Rohenaz <rohenaz@gmail.com>
2020-05-02 08:29:01 -0400

    docker

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # bmap-planaria
 
- A [Planaria](https://github.com/interplanaria) for building 'BMAP' Bitcoin apps. It uses bitbus 2.0 to crawl for transactions and transforms them with bmapjs. It runs two processes:
+A [Planaria](https://github.com/interplanaria) for building 'BMAP' Bitcoin apps. It uses bitbus 2.0 to crawl for transactions and transforms them with bmapjs. It runs two processes:
 
 ## Planaria
-  A crawler and indexer that saves transaction data into a MongoDB database.
- 
- ## Planarium
- A 'Planarium' (REST API) supporting BitQuery syntax.
+
+A crawler and indexer that saves transaction data into a MongoDB database.
+
+## Planarium
+
+A 'Planarium' (REST API) supporting BitQuery syntax.
 
 ![alt text](public/screen.png "Screenshot")
 
@@ -18,48 +20,85 @@
 }
 ```
 
- It then provides support for a number of known OP_RETURN protocols making queries nicer:
+It then provides support for a number of known OP_RETURN protocols making queries nicer:
 
- ```json
+```json
 {
   "find": {
     "BITPIC.paymail": "satchmo@moneybutton.com"
   }
 }
- ```
+```
+
 For a full list of what protocols are supported see [bmapjs.com](https://bmapjs.com)
 
 It also makes working with the results from your frontend much friendlier
 
 ```js
-  let res = await fetch('https://b.map.sv/q/...')
-  let j = res.json()
-  console.log('Got tx', j.tx.h, 'app:', j.MAP.app)
+let res = await fetch("https://b.map.sv/q/...");
+let j = res.json();
+console.log("Got tx", j.tx.h, "app:", j.MAP.app);
 ```
 
 # Install
 
-#### Install Node Dependencies
+### Prerequisites
+
+Docker & Docker-Compose
+
+### Run Locally
+
+Install dependencies
+
 ```bash
 yarn
 ```
 
+Start the app
+
+```bash
+yarn install
+```
+
+### Run in docker
+
+Build the image
+
+```bash
+docker build -t bmap-planaria .
+```
+
+Start the app
+`bash
+docker-compose up
+
+````
+
+### Configuration
+
+`config.js` hold config values.
+
+Set the `from` value to the block to begin indexing.
+
 #### Install MongoDB
+
+To run locally you will need mongodb installed.
 
 See their [installation guide](https://docs.mongodb.com/manual/installation)
 
-
 # Config
+
 Set the following environmental variables:
 
-  - `PLANARIA_TOKEN` Get this from [planaria token](https://token.planaria.network)
-  - `MONGO_URL` A connection string to your mongo database.
-
+- `PLANARIA_TOKEN` Get this from [planaria token](https://token.planaria.network)
+- `MONGO_URL` A connection string to your mongo database.
 
 # Run
+
 ```bash
 node index
-```
+````
+
 # More Examples
 
 [MAP.app = TonicPow](https://b.map.sv/query/ewogICJ2IjogMywKICAicSI6IHsKICAgICJmaW5kIjogewogICAgICAiTUFQLmFwcCI6ICJ0b25pY3BvdyIKICAgIH0sCiAgICAic29ydCI6IHsgImJsay5pIjogLTEgfSwKICAgICJsaW1pdCI6IDEwCiAgfQp9)
@@ -75,10 +114,11 @@ With BitQuery you can search in all sorts of ways.
 1. Set a timestamp in the expected format.
 
 ```js
-let timestamp = Math.floor((new Date().getTime() / 1000) - 86400)
+let timestamp = Math.floor(new Date().getTime() / 1000 - 86400);
 ```
 
 2. Search for records since that timestamp:
+
 ```json
 {
   "v": 3,
@@ -89,6 +129,7 @@ let timestamp = Math.floor((new Date().getTime() / 1000) - 86400)
 ```
 
 # BMAPjs
+
 This Planaria returns data in BMAP format which is a derivative of [BOB](https://github.com/interplanaria/bpu):
 [BMAPjs](https://github.com/rohenaz/bmap)
 
--- planarium.js
+++ planarium.js
@@ -1,83 +1,107 @@
-const express = require('express')
-const app = express()
-const mongo = require('mongodb')
-const { defaultQuery } = require('./queries')
-const chalk = require('chalk')
+const express = require("express");
+const app = express();
+const mongo = require("mongodb");
+const { defaultQuery } = require("./queries");
+const chalk = require("chalk");
 
-process.on('message', async (m, socket) => {
-  console.log('message received!', m, socket)
-  if (m === 'socket') {
-    console.log('m is socket')
-    switch(socket.type) {
-      case 'block':
-        console.log('current block is now', socket.block)
+process.on("message", async (m, socket) => {
+  console.log("message received!", m, socket);
+  if (m === "socket") {
+    console.log("m is socket");
+    switch (socket.type) {
+      case "block":
+        console.log("current block is now", socket.block);
     }
   }
-})
+});
 
-const start = async function() {
-  console.log(chalk.magenta("PLANARIUM"), chalk.cyan("initializing machine..."))
+const start = async function () {
+  console.log(
+    chalk.magenta("PLANARIUM"),
+    chalk.cyan("initializing machine...")
+  );
 
-  app.set('view engine', 'ejs');
-  app.set('views', __dirname + '/views')
-  app.use(express.static(__dirname + '/public'))
-  const port = 3000
-  const host = 'localhost'
-  app.get(/^\/q\/(.+)$/, function(req, res) {
-    let b64= req.params[0]
-    console.log(chalk.magenta('PLANARIUM'), chalk.cyan('query', b64))
-        
-    mongo.MongoClient.connect(process.env.MONGO_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      }, async function(err, db) {
-      if (err) throw err
+  app.set("port", process.env.PORT || 3000);
+  app.set("host", process.env.HOST || "localhost");
+  app.set("view engine", "ejs");
+  app.set("views", __dirname + "/views");
+  app.use(express.static(__dirname + "/public"));
+  app.get(/^\/q\/(.+)$/, function (req, res) {
+    let b64 = req.params[0];
+    console.log(chalk.magenta("PLANARIUM"), chalk.cyan("query", b64));
 
-      var dbo = db.db('bmap')
+    mongo.MongoClient.connect(
+      process.env.MONGO_URL,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      },
+      async function (err, db) {
+        if (err) throw err;
 
-      let code = Buffer
-      .from(b64, 'base64')
-      .toString()
-      let req = JSON.parse(code)
-      dbo.collection('c').find(req.q.find).sort(req.q.sort || {_id:-1}).limit(req.q.limit || 10).project(req.q.project || { in: 0, out: 0 }).toArray(function(err, c) {
-        if (err) throw err
-        dbo.collection('u').find(req.q.find).sort(req.q.sort || {_id:-1}).limit(req.q.limit || 10).project(req.q.project || { in: 0, out: 0 }).toArray(function(err, u) {
-          db.close()
-          res.send({c: c, u: u})
-        })
-      })
-    })
-  })
+        var dbo = db.db("bmap");
 
-  app.get("/query", function(req, res) {
-    let code = JSON.stringify(defaultQuery, null, 2)
-    res.render('explorer', {
-      name: 'BMAP',
+        let code = Buffer.from(b64, "base64").toString();
+        let req = JSON.parse(code);
+        dbo
+          .collection("c")
+          .find(req.q.find)
+          .sort(req.q.sort || { _id: -1 })
+          .limit(req.q.limit || 10)
+          .project(req.q.project || { in: 0, out: 0 })
+          .toArray(function (err, c) {
+            if (err) throw err;
+            dbo
+              .collection("u")
+              .find(req.q.find)
+              .sort(req.q.sort || { _id: -1 })
+              .limit(req.q.limit || 10)
+              .project(req.q.project || { in: 0, out: 0 })
+              .toArray(function (err, u) {
+                db.close();
+                res.send({ c: c, u: u });
+              });
+          });
+      }
+    );
+  });
+
+  app.get("/query", function (req, res) {
+    let code = JSON.stringify(defaultQuery, null, 2);
+    res.render("explorer", {
+      name: "BMAP",
       code: code,
-    })
-  })
+    });
+  });
 
-  app.get(/^\/query\/(.+)$/, function(req, res) {
-    let b64= req.params[0]
-    let code = Buffer.from(b64, 'base64').toString()
-    res.render('explorer', {
-      name: 'BMAP', code: code,
-    })
-  })
+  app.get(/^\/query\/(.+)$/, function (req, res) {
+    let b64 = req.params[0];
+    let code = Buffer.from(b64, "base64").toString();
+    res.render("explorer", {
+      name: "BMAP",
+      code: code,
+    });
+  });
 
-  app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/public/index.html")
-  })
+  app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+  });
 
-  if (host) {
-    app.listen(port, host, () => {
-      console.log(chalk.magenta("PLANARIUM"), chalk.green(`listening on ${host}:${port}!`))
-    })
+  if (app.get("port")) {
+    app.listen(app.get("port"), app.get("host"), () => {
+      console.log(
+        chalk.magenta("PLANARIUM"),
+        chalk.green(`listening on ${app.get("host")}:${app.get("port")}!`)
+      );
+    });
   } else {
-    app.listen(port, () => {
-      console.log(chalk.magenta("PLANARIUM"), chalk.green(`listening on port ${port}!`))
-    })
+    app.listen(app.get("port"), () => {
+      console.log(
+        chalk.magenta("PLANARIUM"),
+        chalk.green(`listening on port ${app.get("port")}!`)
+      );
+    });
   }
-}
+};
 
-start()
+start();
