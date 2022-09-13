3e76b0dc9743bc3ac609e9810c855b4c65eca7c0
Luke <rohenaz@gmail.com>
2022-09-13 10:29:08 -0400

    wip

--- crawler.ts
+++ crawler.ts
@@ -69,7 +69,7 @@ const crawl = (query, height) => {
 
     const subscription = jungleBusClient.Subscribe(
       subId,
-      height, 
+      currentBlock || height, 
       async function onPublish(ctx) {
         // transaction found
         // console.log({ctx});
@@ -91,98 +91,19 @@ const crawl = (query, height) => {
         }
       },
       function onBlockDone(cMsg) {
+       
         // add your own code here
-        setCurrentBlock(cMsg.block_height)
+        setCurrentBlock(cMsg.block)
         console.log(
           chalk.blue('####  '),
           chalk.magenta('NEW BLOCK '),
           chalk.green(currentBlock)
         )
         // planarium.send('socket', { type: 'block', block: currentBlock })
-        console.log({cMsg});
+        // console.log({cMsg});
       });
       
     subscription.Subscribe();
-
-    // // only block indexes greater than given height
-    // query.q.find['blk.i'] = { $gt: height }
-
-    // let res
-    // try {
-    //   res = await fetch('https://bob.bitbus.network/block', {
-    //     method: 'post',
-    //     headers: {
-    //       'Content-type': 'application/json; charset=utf-8',
-    //       token: config.token,
-    //     },
-    //     body: JSON.stringify(query),
-    //   })
-    // } catch (e) {
-    //   console.error('Failed to reach bitbus', e)
-    //   reject()
-    //   return
-    // }
-
-    // // The promise is resolved when the stream ends.
-    // res.body
-    //   .on('end', () => {
-    //     resolve()
-    //   })
-    //   // Split NDJSON into an array stream
-    //   .pipe(es.split())
-    //   // Apply the logic for each line
-    //   .pipe(
-    //     es.mapSync(async (t) => {
-    //       if (t) {
-    //         let j
-    //         try {
-    //           j = JSON.parse(t)
-    //         } catch (e) {
-    //           // Invalid response
-    //           console.error('Invalid response', e, t)
-    //           return null
-    //         }
-    //         if (!j) {
-    //           console.log('Invalid response', j)
-    //           return
-    //         }
-    //         // New block
-    //         if (j.blk && j.blk.i > currentBlock) {
-    //           setCurrentBlock(j.blk.i)
-    //           console.log(
-    //             chalk.blue('####  '),
-    //             chalk.magenta('NEW BLOCK '),
-    //             chalk.green(currentBlock)
-    //           )
-    //           // planarium.send('socket', { type: 'block', block: currentBlock })
-    //         }
-
-    //         //             // Extract BitFS URIs
-    //         //             // Iterate through all outputs and find chunks whose names start with "f"
-    //         let bitfs = []
-    //         if (j.out) {
-    //           j.out.forEach((out) => {
-    //             for (let tape of out.tape) {
-    //               let cell = tape.cell
-    //               for (let pushdata of cell) {
-    //                 if (pushdata.hasOwnProperty('f')) {
-    //                   bitfs.push(pushdata.f)
-    //                 }
-    //               }
-    //             }
-    //           })
-    //         }
-    //         // Crawl BitFS
-    //         saveFiles(bitfs)
-
-    //         try {
-    //           return await saveTx(j)
-    //         } catch (e) {
-    //           return null
-    //         }
-    //       }
-    //     })
-    //   )
   })
 }
 
