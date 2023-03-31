d09f8b4a8ac25e7f8842555518181e8a1b0411c2
Luke Rohenaz <rohenaz@gmail.com>
2023-03-30 20:58:09 -0400

    add bmapjs tx endpoint

--- api.ts
+++ api.ts
@@ -1,6 +1,8 @@
 import { Transaction } from '@gorillapool/js-junglebus'
+import { allProtocols, TransformTx } from 'bmapjs'
 import { BmapTx } from 'bmapjs/types/common.js'
 import bodyParser from 'body-parser'
+import { parse } from 'bpu-ts'
 import chalk from 'chalk'
 import cors from 'cors'
 import express from 'express'
@@ -240,6 +242,125 @@ const start = async function () {
       )
     })
   }
+}
+
+app.get(
+  '/tx',
+  asyncHandler(async (req, res) => {
+    const tx = req.params[0]
+    const format = req.params[0]
+
+    console.log('tx', tx)
+    // fetch the tx
+    try {
+      if (format === 'raw') {
+        const rawTx = await rawTxFromTxid(tx)
+        res.status(200).send(rawTx)
+        return
+      } else if (format === 'json') {
+        const json = await jsonFromTxid(tx)
+        res.status(200).send(json)
+        return
+      }
+      const bob = await bobFromTxid(tx)
+      console.log('bob', bob.out[0])
+      // Transform from BOB to BMAP
+      console.log('loading protocols', allProtocols)
+      const decoded = await TransformTx(
+        bob,
+        allProtocols.map((p) => p.name)
+      )
+      console.log('bmap', decoded)
+      // Response (segment and formatting optional)
+      res
+        .status(200)
+        .send(
+          format === 'bob'
+            ? bob
+            : format === 'bmap'
+            ? decoded
+            : format && decoded[format]
+            ? decoded[format]
+            : format && format.length
+            ? `Key ${format} not found in tx`
+            : `<pre>${JSON.stringify(decoded, undefined, 2)}</pre>`
+        )
+    } catch (e) {
+      res.status(400).send('Failed to process tx ' + e)
+    }
+  })
+)
+
+const bobFromRawTx = async (rawtx: string) => {
+  return await parse({
+    tx: { r: rawtx },
+    split: [
+      {
+        token: { op: 106 },
+        include: 'l',
+      },
+      {
+        token: { s: '|' },
+      },
+    ],
+  })
+}
+const bobFromPlanariaByTxid = async (txid: string) => {
+  // // The query we constructed from step 2.
+  const query = {
+    v: 3,
+    q: {
+      find: {
+        'tx.h': txid,
+      },
+      sort: {
+        'blk.i': -1,
+        i: -1,
+      },
+      limit: 1,
+    },
+  }
+  // Turn the query into base64 encoded string.
+  const b64 = Buffer.from(JSON.stringify(query)).toString('base64')
+  const url = `https://bob.planaria.network/q/1GgmC7Cg782YtQ6R9QkM58voyWeQJmJJzG/${b64}`
+  // Attach planaria API KEY as header
+  const header = {
+    headers: { key: '14yHvrKQEosfAbkoXcEwY6wSvxNKteFbzU' },
+  }
+  const res = await fetch(url, header)
+  const j = await res.json()
+  return j.c.concat(j.u)[0]
+}
+const jsonFromTxid = async (txid: string) => {
+  // get rawtx for txid
+  const url = 'https://api.whatsonchain.com/v1/bsv/main/tx/' + txid
+  console.log('hitting', url)
+  // let res = await fetch(url, header)
+  const res = await fetch(url)
+  return await res.json()
+}
+const bobFromTxid = async (txid: string) => {
+  const rawtx = await rawTxFromTxid(txid)
+  // Transform using BPU
+  try {
+    return await bobFromRawTx(rawtx)
+  } catch (e) {
+    console.log(
+      'Failed to ger rawtx from whatsonchain for.',
+      txid,
+      'Failing back to BOB planaria.',
+      e
+    )
+    return await bobFromPlanariaByTxid(txid)
+  }
+}
+const rawTxFromTxid = async (txid: string) => {
+  // get rawtx for txid
+  const url = 'https://api.whatsonchain.com/v1/bsv/main/tx/' + txid + '/hex'
+  console.log('hitting', url)
+  // let res = await fetch(url, header)
+  const res = await fetch(url)
+  return await res.text()
 }
 
 start()
