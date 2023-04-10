1a7e23f789eda8c4d7739254fc8006e349fc76fa
Luke <rohenaz@gmail.com>
2023-04-10 12:17:45 -0400

    add pm2 script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -16,8 +16,7 @@ module.exports = {
       repo: 'GIT_REPOSITORY',
       path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
-      'post-deploy':
-        'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'yarn && pm2 reload ecosystem.config.cjs --env production',
       'pre-setup': '',
     },
   },
