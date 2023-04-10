c98ee4da42e3e9a816e00e11e1869145ef8a539c
Luke <rohenaz@gmail.com>
2023-04-10 12:11:24 -0400

    add pm2 script

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: 'BMAP API',
+      script: 'index.js',
+      watch: '.',
+      node_args: '--openssl-legacy-provider --no-experimental-fetch',
+    },
+    {
+      script: './service-worker/',
+      watch: ['./service-worker'],
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
+}
