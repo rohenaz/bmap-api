8b51a5df0ac2a280c52105b1e6bd6f75e2d02a4a
Luke Rohenaz <rohenaz@gmail.com>
2023-10-15 17:47:24 -0400

    b64 links

--- dash.ts
+++ dash.ts
@@ -1,12 +1,18 @@
 import QuickChart from 'quickchart-js'
-
+function collectionQuery(collection: string) {
+  return Buffer.from((defaultQuery.q.find['MAP.type'] = collection)).toString(
+    'base64'
+  )
+}
 function getGridItemsHtml(
   collection: string,
   count: number,
   chart: QuickChart
 ) {
   return `
-<a href='/query/${encodeURIComponent(collection)}'>
+<a href='/query/${encodeURIComponent(collection)}/${collectionQuery(
+    collection
+  )}'>
   <div class='border border-zinc-700 p-4 text-center dark:bg-zinc-800 dark:text-white'>
     <div class='text-lg font-semibold dark:text-white flex justify-between'>
       ${collection}
