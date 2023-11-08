d87e89cbe55893cc269c88c59716d40cd54030db
Luke Rohenaz <rohenaz@gmail.com>
2023-11-08 17:10:02 -0500

    parse identity

--- bap.ts
+++ bap.ts
@@ -66,7 +66,10 @@ export const getBAPIdByAddress = async (
     console.log('identity data', { data })
     if (data && data.status === 'OK' && data.result) {
       try {
-        return JSON.parse(data.result)
+        if (data.result.identity) {
+          data.result.identity = JSON.parse(data.result.identity)
+        }
+        return data.result
       } catch (e) {
         console.log('Failed to parse BAP identity', e, data.result)
       }
