61e973957b879cbecce7b95f9c44ab88f29a843c
Luke Rohenaz <rohenaz@gmail.com>
2023-11-08 15:52:27 -0500

    cache

--- cache.ts
+++ cache.ts
@@ -115,8 +115,8 @@ async function wasIngested(txid: string): Promise<boolean> {
 async function cacheIngestedTxid(txid: string): Promise<void> {
   const cachedValue = await readFromRedis<CacheIngest>('ingest')
   let ingestCache = cachedValue ? cachedValue.value : []
-  if (!ingestCache?.includes(txid)) {
-    ingestCache = [...ingestCache, txid]
+  if (!ingestCache || !ingestCache.includes(txid)) {
+    ingestCache = ingestCache ? [...ingestCache, txid] : [txid]
     await saveToRedis('ingest', { type: 'ingest', value: ingestCache })
   }
 }
