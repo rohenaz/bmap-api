3da3fd37bfb2619c465d18e24b64cdc8d534552e
Satchmo <rohenaz@gmail.com>
2025-01-25 19:37:59 -0500

    update git ignore

--- playground.mongodb.js
+++
@@ -1,9 +0,0 @@
-// For messages (if needed)
-const messageQuery = {
-  'MAP.type': 'message',
-  'MAP.bapID': 'Go8vCHAa4S6AhXKTABGpANiz35J',
-};
-
-print('\nMessage Relationships:');
-const messageRelationships = db.message.find(messageQuery).toArray();
-printjson(messageRelationships.map((msg) => msg.MAP));
